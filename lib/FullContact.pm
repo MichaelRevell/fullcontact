e1a70072aea1412dfbae8d609ee5f3826ceffa57
Michael Revell <mikearevell@gmail.com>
2013-04-28 19:57:59 -0700

    error handling

--- lib/FullContact.pm
+++ lib/FullContact.pm
@@ -3,66 +3,18 @@ use LWP::Simple;
 use JSON;
 use Data::Dumper;
 use Template;
+use Try::Tiny;
 use File::Slurp;
 use DBI;
-#use strict;
-use warnings;
 use Dancer ':syntax';
+use strict;
+use warnings FATAL => 'all';
 
 our $VERSION = '0.1';
 
-sub connect_db {
-  my $db = './fullcontact.db';
-  my $dbh = DBI->connect("dbi:SQLite:dbname=$db") or die $DBI::errstr;
 
-  return $dbh;
-}
 
-sub init_db {
-  my $db = connect_db();
-  my $schema = read_file('./schema.sql');
-  $db->do($schema) or die $db->errstr;
-}
-
-sub insert_user {
-  local($a, $b);
-  ($a, $b) = ($_[0], $_[1]);
-  my $db = connect_db();
-  my $sql = 'insert into users (email, json) values (?, ?)';
-  my $sth = $db->prepare($sql) or die $db->errstr;
-  $sth->execute($a, $b) or die $sth->errstr;
-}
-
-sub select_user {
-  local($a, $b);
-  ($a, $b) = ($_[0], $_[1]);
-  my $db = connect_db();
-  my $sql = "select id, email, json from users where email='$a' limit 1";
-  my $sth = $db->prepare($sql) or die $db->errstr;
-  $sth->execute or die $sth->errstr;
-  my $data = $sth->fetchall_hashref('email');
-  return $data->{"$a"};
-}
-sub select_user_id {
-  my $id;
-  $id = $_[0];
-  my $db = connect_db();
-  my $sql = "select id, email, json from users where id='$id' limit 1";
-  my $sth = $db->prepare($sql) or die $db->errstr;
-  $sth->execute or die $sth->errstr;
-  my $data = $sth->fetchall_hashref('id');
-  return $data->{"$id"};
-}
-
-sub select_users {
-  my $db = connect_db();
-  my $sql = "select id, email, json from users order by id desc";
-  my $sth = $db->prepare($sql) or die $db->errstr;
-  $sth->execute or die $sth->errstr;
-  my $data = $sth->fetchall_arrayref;
-  return $data;
-}
-
+# Routes
 get '/' => sub {
     template 'index';
 };
@@ -88,14 +40,17 @@ post '/users/create' => sub {
   my $id = $user->{'id'};
   $user_email = $user->{'email'};
   if(!defined $user_email ) {
-    #$json = read_file("/Users/mars/Sites/dancer/FullContact/testdata.json");
-    my $json = LWP::Simple::get("https://api.fullcontact.com/v2/person.json?email=$email&apiKey=e664d3674f971206");
-    print ref($json);
-    insert_user($email, $json);
-    $user  = select_user($email);
-    $id = $user->{'id'};
+      my $json = LWP::Simple::get("https://api.fullcontact.com/v2/person.json?email=$email&apiKey=e664d3674f971206");
+      insert_user($email, $json);
+      $user  = select_user($email);
+      $id = $user->{'id'};
   }
-  redirect "/users/$id"
+  if(!defined $id){
+    redirect "/error";
+  }
+  else {
+    redirect "/users/$id"
+  }
 };
 
 get '/users/:id' => sub {
@@ -137,6 +92,70 @@ get '/users/:id/json' => sub {
   }
   print Dumper $content;
 };
+
+get "/error" => sub {
+  template "error";
+};
+
+
+# Database Calls
+sub connect_db {
+  my $db = './fullcontact.db';
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$db") or die $DBI::errstr;
+
+  return $dbh;
+}
+
+sub init_db {
+  my $db = connect_db();
+  my $schema = read_file('./schema.sql');
+  $db->do($schema) or die $db->errstr;
+}
+
+sub insert_user {
+  local($a, $b);
+  ($a, $b) = ($_[0], $_[1]);
+  my $db = connect_db();
+  my $sql = 'insert into users (email, json) values (?, ?)';
+  my $sth = $db->prepare($sql);
+  local $SIG{__WARN__} = sub {};
+  try {
+    $sth->execute($a, $b) or die;
+    #die;
+  } catch {
+    redirect '/error';
+  };
+}
+
+sub select_user {
+  local($a, $b);
+  ($a, $b) = ($_[0], $_[1]);
+  my $db = connect_db();
+  my $sql = "select id, email, json from users where email='$a' limit 1";
+  my $sth = $db->prepare($sql) or die $db->errstr;
+  $sth->execute or die $sth->errstr;
+  my $data = $sth->fetchall_hashref('email');
+  return $data->{"$a"};
+}
+sub select_user_id {
+  my $id;
+  $id = $_[0];
+  my $db = connect_db();
+  my $sql = "select id, email, json from users where id='$id' limit 1";
+  my $sth = $db->prepare($sql) or die $db->errstr;
+  $sth->execute or die $sth->errstr;
+  my $data = $sth->fetchall_hashref('id');
+  return $data->{"$id"};
+}
+
+sub select_users {
+  my $db = connect_db();
+  my $sql = "select id, email, json from users order by id desc";
+  my $sth = $db->prepare($sql) or die $db->errstr;
+  $sth->execute or die $sth->errstr;
+  my $data = $sth->fetchall_arrayref;
+  return $data;
+}
 
 init_db();
 true;
